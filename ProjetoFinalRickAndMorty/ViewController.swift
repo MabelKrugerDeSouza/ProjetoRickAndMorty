5bfdffd6022cbd9f7d3bfa0e1f9c9561f59e13f6
Mabel <mabelksouza@gmail.com>
2021-10-26 16:42:39 -0300

    Initial Commit

---
+++ ProjetoFinalRickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjetoFinalRickAndMorty
+//
+//  Created by Mabel Kruger de Souza on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
